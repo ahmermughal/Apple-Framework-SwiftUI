8014cab7f1606c3b6438651e80ba3947c7c2df62
ahmermughal <ahmeramughal@gmail.com>
2024-03-24 17:20:29 +0100

    Initial Commit

---
+++ Apple-Frameworks-SwiftUI/Apple_Frameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_SwiftUIApp.swift
+//  Apple-Frameworks-SwiftUI
+//
+//  Created by Ahmer Mughal on 24.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
